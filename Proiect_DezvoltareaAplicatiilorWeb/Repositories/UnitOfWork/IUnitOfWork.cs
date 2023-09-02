5530d8325e857b2288d226f724b953f55682fe8f
Destro25 <alexandrualin1010@gmail.com>
2023-09-02 19:09:59 +0300

    Added UnitOfWork

---
+++ Proiect_DezvoltareaAplicatiilorWeb/Repositories/UnitOfWork/IUnitOfWork.cs
@@ -0,0 +1,18 @@
+﻿using Proiect_DezvoltareaAplicatiilorWeb.Repositories.AddressRepository;
+using Proiect_DezvoltareaAplicatiilorWeb.Repositories.MovieRentalRepository;
+using Proiect_DezvoltareaAplicatiilorWeb.Repositories.MovieRepository;
+using Proiect_DezvoltareaAplicatiilorWeb.Repositories.RentalRepository;
+using Proiect_DezvoltareaAplicatiilorWeb.Repositories.UserRepository;
+
+namespace Proiect_DezvoltareaAplicatiilorWeb.Repositories.UnitOfWork
+{
+    public interface IUnitOfWork
+    {
+        Task<bool> SaveAsync();
+        IUserRepository userRepository { get; }
+        IAddressRepository addressRepository { get; }
+        IMovieRepository movieRepository { get; }
+        IMovieRentalRepository movieRentalRepository { get; }
+        IRentalRepository rentalRepository { get; }
+    }
+}
---
+++ Proiect_DezvoltareaAplicatiilorWeb/Repositories/UnitOfWork/UnitOfWork.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.Data.SqlClient;
+using Proiect_DezvoltareaAplicatiilorWeb.Data;
+using Proiect_DezvoltareaAplicatiilorWeb.Repositories.AddressRepository;
+using Proiect_DezvoltareaAplicatiilorWeb.Repositories.MovieRentalRepository;
+using Proiect_DezvoltareaAplicatiilorWeb.Repositories.MovieRepository;
+using Proiect_DezvoltareaAplicatiilorWeb.Repositories.RentalRepository;
+using Proiect_DezvoltareaAplicatiilorWeb.Repositories.UserRepository;
+
+namespace Proiect_DezvoltareaAplicatiilorWeb.Repositories.UnitOfWork
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        public IUserRepository userRepository { get; set; }
+        public IAddressRepository addressRepository { get; set; }
+        public IMovieRepository movieRepository { get; set; }
+        public IMovieRentalRepository movieRentalRepository { get; set; }
+        public IRentalRepository rentalRepository { get; set; }
+
+        private ProjectContext ProjectContext { get; set; }
+
+        public UnitOfWork(IUserRepository userRepository, IAddressRepository addressRepository, IMovieRepository movieRepository, IMovieRentalRepository movieRentalRepository, IRentalRepository rentalRepository, ProjectContext projectContext)
+        {
+            this.userRepository = userRepository;
+            this.addressRepository = addressRepository;
+            this.movieRepository = movieRepository;
+            this.movieRentalRepository = movieRentalRepository;
+            this.rentalRepository = rentalRepository;
+            this.ProjectContext = projectContext;
+        }
+
+        public async Task<bool> SaveAsync()
+        {
+            try
+            {
+                return await ProjectContext.SaveChangesAsync() > 0;
+            }
+            catch (SqlException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            return false;
+        }
+    }
+}
