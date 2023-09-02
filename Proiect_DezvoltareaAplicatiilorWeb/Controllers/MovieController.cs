41ce516019eacb6fd2a302f3fdf71bf14ff87ef4
Destro25 <alexandrualin1010@gmail.com>
2023-09-03 01:05:53 +0300

    Controllers Done

---
+++ Proiect_DezvoltareaAplicatiilorWeb/Controllers/MovieController.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Proiect_DezvoltareaAplicatiilorWeb.Models.DTOs;
+using Proiect_DezvoltareaAplicatiilorWeb.Services.MovieService;
+
+namespace Proiect_DezvoltareaAplicatiilorWeb.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MovieController : ControllerBase
+    {
+        public readonly IMovieService movieService;
+
+        public MovieController(IMovieService movieService) 
+        {
+            this.movieService = movieService;
+        }
+
+        [HttpPut("update-movie")]
+        //[Authorization(Role.Admin)]
+        public async Task<IActionResult> UpdateMovie(MovieDTO _movie, Guid movieId)
+        {
+            var movie = await movieService.Update(movieId, _movie);
+            if (movie == null) 
+            {
+                return NotFound();
+            }
+            return Ok(movie);
+        }
+
+        [HttpGet("get-all-movies")]
+        //[Authorization(Role.Admin)]
+        public async Task<IActionResult> GetAllMovies()
+        {
+            return Ok(await movieService.GetAll());
+        }
+
+        [HttpPost("add-a-movie")]
+        public async Task<IActionResult> AddMovie(MovieDTO _movie)
+        {
+            await movieService.Create(_movie);
+            return Ok();
+        }
+
+        [HttpDelete("delete-movie")]
+        //[Authorization(Role.Admin)]
+        public async Task<IActionResult> DeleteMovie(Guid Id)
+        {
+            await movieService.Delete(Id);
+            return Ok("Movie was succesfully deleted.");
+        }
+
+        [HttpGet("get-movies-by-studio")]
+        public async Task<IActionResult> GetMovieByStudio(string Studio)
+        {
+            var movies = movieService.GetByStudio(Studio);
+            return Ok(movies);
+        }
+    }
+}
---
+++ Proiect_DezvoltareaAplicatiilorWeb/Controllers/RentalController.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Proiect_DezvoltareaAplicatiilorWeb.Models.DTOs;
+using Proiect_DezvoltareaAplicatiilorWeb.Services.RentalService;
+
+namespace Proiect_DezvoltareaAplicatiilorWeb.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RentalController : ControllerBase
+    {
+        public readonly IRentalService _rentalService;
+
+        public RentalController(IRentalService rentalService)
+        {
+            _rentalService = rentalService;
+        }
+
+        [HttpPut("update-rental")]
+        //[Authorization(Role.Admin)]
+        public async Task<IActionResult> UpdateRental(RentalDTO rent, Guid rentalId)
+        {
+            var rental = await _rentalService.Update(rentalId, rent);
+            if (rental == null)
+            {
+                return NotFound();
+            }
+            return Ok(rental);
+        }
+
+        [HttpGet("get-all-rentals")]
+        //[Authorization(Role.Admin)]
+        public async Task<IActionResult> GetAllRentals()
+        {
+            return Ok(_rentalService.GetAll());
+        }
+
+        [HttpPost("make-a-rental")]
+        //[Authorization(Role.Admin)]
+        public async Task<IActionResult> AddRental(RentalDTO rent)
+        {
+            await _rentalService.Create(rent);
+            return Ok(rent);
+        }
+
+        [HttpDelete("delete-rental")]
+        //[Authorization(Role.Admin)]
+        public async Task<IActionResult> DeleteRental(Guid Id)
+        {
+            await _rentalService.Delete(Id);
+            return Ok("Rental was succesfully deleted.");
+        }
+
+        /*
+        [HttpGet("get-rentals-and-movies")]
+        public async Task<IActionResult> GetRentsAndMovs()
+        {
+            var rents = _rentalService.GetAllMovieRentals();
+            return Ok(rents);
+        }
+        */
+    }
+}
